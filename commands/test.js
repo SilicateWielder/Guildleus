ccec67361a79e89b96867157238407a9124f500d
SilicateWielder <oculartide@gmail.com>
2020-04-26 20:48:29 -0500

    Implemented process creation, misc
    - Implemented process creation
    - Fixed bug disabling mouse on view-switching

--- commands/test.js
+++ commands/test.js
@@ -11,11 +11,14 @@ exports.init = function() {
 exports.run = async function(client, data) {
 	let message = data.message;
 	
-	// Calculates ping between sending a message and editing it, giving a nice round-trip latency.
-	// The second ping is an average latency between the bot and the websocket server (one-way, not round-trip)
+	let process = processManager.create();
 	
-	const m = await message.channel.send("Ping?");
-	m.edit(`Pong! Latency is ${m.createdTimestamp - data.message.createdTimestamp}ms.`);
+	if(typeof process == 'object')
+	{
+		message.channel.send("Created new process with ID: " + process.id);
+	} else {
+		message.channel.send("ERROR: " + process);
+	}
 }
 
 exports.queries = [
