506006c977dde4ead7eaa1d386619f01e17eaeb0
SilicateWielder <oculartide@gmail.com>
2020-04-28 04:11:45 -0500

    base implement of PML
    The PML is the process management loop, work has begun on the main process handler. This is currently performed as a node-cronjob running at a per-second inverval.

--- commands/test.js
+++ commands/test.js
@@ -8,21 +8,27 @@ exports.init = function() {
 	return 0;
 }
 
-exports.run = async function(client, data) {
+exports.run = async function(command, data) {
 	let message = data.message;
+	let args = command.args;
 	
-	function sample (message = data.message) {
-		message.channel.send("babump");
-	}
+	if(args[0] == 'create')
+		{
+		function sample (message = data.message) {
+			message.channel.send("babump");
+		}
 
-	let endTime = new DateSignature();
-	let process = processManager.create(data, 15, endTime, sample);
-	
-	if(typeof process == 'object')
-	{
-		message.channel.send("Created new process with ID: " + process.id);
-	} else {
-		message.channel.send("ERROR: " + process);
+		let endTime = new DateSignature();
+		let process = processManager.create(data, 15, endTime, sample);
+		
+		if(typeof process == 'object')
+		{
+			message.channel.send("Created new process with ID: " + process.id);
+		} else {
+			message.channel.send("ERROR: " + process);
+		}
+	} else if (args[0] == 'delete' && args[1] != undefined) {
+		processManager.remove(args[1]);
 	}
 }
 
